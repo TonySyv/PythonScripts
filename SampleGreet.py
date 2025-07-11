5e8fb0f350da7cee0c211d9507d14e86af2b5a3e
Tony Syv <11375364+TonySyv@users.noreply.github.com>
2025-07-11 21:21:23 +0800

    Create SampleGreet.py

---
+++ SampleGreet.py
@@ -0,0 +1,6 @@
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to my GitHub profile.")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet_user(user_name)
